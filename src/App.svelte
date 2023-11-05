b8edb175a19166fe89f93841ee4b9b4b9a8f418b
ssssota <tommysouta@gmail.com>
2023-11-05 13:07:26 +0900

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,74 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  import { onMount } from "svelte";
+
+  let devicesPromise: Promise<MediaDeviceInfo[]> = new Promise(() => {});
+
+  let selected = false;
+  let videoId: string | undefined;
+  let audioId: string | undefined;
+
+  let video: HTMLVideoElement;
+
+  onMount(() => {
+    devicesPromise = navigator.mediaDevices.enumerateDevices();
+  });
+
+  const onSubmit = () => {
+    selected = true;
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: { ideal: 1920, max: 3840 },
+          height: { ideal: 1080, max: 2160 },
+          deviceId: videoId,
+        },
+        audio: {
+          deviceId: audioId,
+        },
+      })
+      .then((stream) => {
+        video.srcObject = stream;
+      });
+  };
 </script>
 
-<main>
-  <h1>Vite + Svelte</h1>
+{#if !selected}
+  <form on:submit|preventDefault={onSubmit}>
+    {#await devicesPromise}
+      Loading devices...
+    {:then devices}
+      <label>
+        Video:
+        <select bind:value={videoId}>
+          {#each devices.filter((d) => d.kind === "videoinput") as device}
+            <option value={device.deviceId}>{device.label}</option>
+          {/each}
+        </select>
+      </label>
+      <label>
+        Audio:
+        <select bind:value={audioId}>
+          {#each devices.filter((d) => d.kind === "audioinput") as device}
+            <option value={device.deviceId}>{device.label}</option>
+          {/each}
+        </select>
+      </label>
+    {:catch error}
+      {error.message}
+    {/await}
+    <button type="submit">Submit</button>
+  </form>
+{:else}
+  <video bind:this={video} controls autoplay>
+    <p>Video stream not available.</p>
+    <track kind="captions" />
+  </video>
+{/if}
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+<style>
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+</style>
